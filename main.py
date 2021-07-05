885de674446398146febe605070fdf1786bd8971
Rishika Vadlamudi <70661830+rishika-v@users.noreply.github.com>
2021-07-05 16:04:02 +0000

    added print statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 name = input("Enter your name: ")
-print("Hi, " + name + " let's get started")
+print("Hi " + name + ", let's get started")
 
 print(" ")
 
@@ -8,3 +8,6 @@ adj1 = input("Enter an Adjective: ")
 food = input("Enter a Noun (plural): ")
 v1 = input("Enter a Verb: ")
 person = input("Enter a noun (person or animal): ")
+
+
+print("I'm going to "  + place + " to eat " + adj1 + " " + food)
